d3f28dd1740207f368ccd8b4f5222c03ac619d11
ThibautE <thibaut07600@gmail.com>
2018-03-05 14:15:25 +0100

    First Commit, and probably the last

--- app/src/main/java/com/example/thibaut/smartcity/MainActivity.java
+++ app/src/main/java/com/example/thibaut/smartcity/MainActivity.java
@@ -6,8 +6,15 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     private TextView mTextMessage;
 
@@ -19,12 +26,6 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -39,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void changeActivity (View view){
+        Intent intent = new Intent (this, BurgerMenu.class);
+        startActivity(intent);
     }
 
 }
